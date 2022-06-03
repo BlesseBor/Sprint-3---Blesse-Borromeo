7e17f8677dc25db0c41e08dd2413c7224b105901
BlesseBor <97005828+BlesseBor@users.noreply.github.com>
2022-06-03 10:09:42 +0800

    Add files via upload

---
+++ Database/contact.php
@@ -0,0 +1,18 @@
+<?php
+// database connection code
+if(isset($_POST['name']))
+{
+// connection
+$con = mysqli_connect('localhost', 'root', '','db_connect');
+
+// to get the data
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// insert the data to the database
+$sql = "INSERT INTO bio_contact (id, name, email, message) VALUES ('0', '$name', '$email', '$message')";
+
+$rs = mysqli_query($con, $sql);
+?>
